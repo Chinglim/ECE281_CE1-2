7510e4f4742c5d13296b752cad77ef7294a4fa93
Alex Sisco <C16alex.sisco@usafa.edu>
2014-01-06 16:59:43 -0700

    added waveform image

--- CE1_shell.vhd
+++ CE1_shell.vhd
@@ -29,8 +29,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
---test comment for GIT
-
 entity CE1_shell is
     Port ( A : in  STD_LOGIC;
            B : in  STD_LOGIC;
